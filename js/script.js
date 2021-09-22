14925d9520f110b4099a0c5e5287a81570a937f5
TomDeeDee <tom.derdaele@brederographics.com>
2021-09-22 19:39:02 +0200

    add js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-//JavaScript komt hier ...
+"use strict";
+
+const title = document.getElementById("titleHeader");
+const paragraph = document.getElementById("p1");
+const paragraph2 = document.getElementById("p2");
+
+const name1 = "Tom";
+let locatie;
+locatie = "Howest Brugge";
+let message = "einde oefening";
+
+//title contains name
+
+title.innerText = `Hello ${name1}`;
+paragraph.innerText = `Welkom in ${locatie}`;
+paragraph2.innerText = message;
